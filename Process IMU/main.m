df9680dd37eeed13762fe4007d66ab6a6f6d1a70
ahmedag91 <ahmed.abdulgawad91@gmail.com>
2020-10-20 21:19:07 +0200

    Updated read_and_preprocess and main

--- Process IMU/main.m
+++ Process IMU/main.m
@@ -1,7 +1,7 @@
 close all
 clear
 clc
-read_and_Preprocess;
+[ax_earth, ay_earth, az_earth, time_stamps, dt] = read_and_Preprocess();
 %% crop the data and time stamps to your chosen subinterval
 
 start_date = [toStringJSON(time_stamps(1).Year), '-', toStringJSON(time_stamps(1).Month), '-', toStringJSON(time_stamps(1).Day)];
@@ -16,16 +16,9 @@ interval_start = datetime({[ start_date, start_time ]},'Format','yyyy-MM-dd''T''
 [time_stamps, ax_earth, ay_earth, az_earth] = cropData(time_stamps, ax_earth, ay_earth, az_earth, start_date, start_time, interval_length);
 t = (0:length(ax_earth)-1)*dt;
 
-plotting(time_stamps, [ax_earth; ay_earth; az_earth], 'Earth aceelerations before cropping',....
+plotting(time_stamps, [ax_earth; ay_earth; az_earth], 'Earth aceelerations after cropping',....
  {'$a_x(t)$ (Earth)', '$a_z(t)$ (Earth)', '$a_z(t)$ (Earth)'},.....
   'Time, $t$~(s)', 'Accelerations~($m/s^{2}$)')
-
-figure
-hold on;
-title('Earth aceelerations before cropping','Fontsize',16,'interpreter','Latex')
-q = plot (time_stamps, [ax_earth; ay_earth; az_earth]);
-set(legend(q,'$a_x$ (Earth)', '$a_y$ (Earth)', '$a_z$ (Earth)'),'interpreter','latex','fontsize',20)
-grid on;set(gca,'fontsize',16) ;box on
 
 %% Compute the drift-free velocities
 
@@ -48,35 +41,3 @@ plotting(t, [x; z], 'Displacements after detrending',...
  {'$x(t)$ (Earth)', '$z(t)$ (Earth)'}, 'Time, $t$~(s)', 'Displacements~(m)')
 
 
-
-
-
-
-
-function plotting(xInput, yInput, tit, legends, Xlabel, Ylabel)
-% plotting - Description
-% This is a plotting function for not repeating myself
-%
-%
-%
-%
-% Syntax: plotting(xInput, yInput, tit, legends, Xlabel, Ylabel)
-% xInput: x-axis input (vector)
-% yInput: y-axis input vector or matrix
-% tit: figure title (string)
-% legends: cell array of strings describing the legends 
-% Xlabel: x-axis label
-% Ylabel: y-axis label
-%
-% Long description
-    figure
-    hold on
-    title(tit, 'Fontsize', 16, 'interpreter', 'Latex');
-    q = plot(xInput, yInput);
-    set(legend(q,legends),'interpreter','latex','fontsize',20);
-    xlabel(Xlabel, 'Interpreter', 'latex','FontSize', 20)
-    ylabel(Ylabel, 'Interpreter', 'latex','FontSize', 20)
-    grid on;
-    set(gca,'fontsize',16);
-    box on
-end
